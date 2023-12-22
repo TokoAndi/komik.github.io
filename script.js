0bbde0659f1d2d1171d6c4428e635f4e8931693a
TokoAndi <139789681+TokoAndi@users.noreply.github.com>
2023-12-22 19:19:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-let komikData = JSON.parse(localStorage.getItem('komikData')) || [];
+let komikData = JSON.parse(localStorage.getItem("komikData")) || [];
 
 function tambahData() {
-    const namaKomik = document.getElementById('namaKomik').value;
-    const chapter = document.getElementById('chapter').value;
+  const namaKomik = document.getElementById("namaKomik").value;
+  const chapter = document.getElementById("chapter").value;
 
-    if (namaKomik && chapter) {
-        komikData.push({ namaKomik, chapter });
-        updateData();
-        resetForm();
-        simpanDataLocal();
-    }
+  if (namaKomik && chapter) {
+    komikData.push({ namaKomik, chapter });
+    updateData();
+    resetForm();
+    simpanDataLocal();
+  }
 }
 
-function updateData() {
-    const dataContainer = document.getElementById('dataContainer');
-    dataContainer.innerHTML = '<h2>Data:</h2>';
+function updateData(data = komikData) {
+  const dataContainer = document.getElementById("dataContainer");
+  dataContainer.innerHTML = "<h2>Data:</h2>";
 
-    if (komikData.length === 0) {
-        dataContainer.innerHTML += '<p>Data Tidak Ditemukan</p>';
-        return;
-    }
+  if (data.length === 0) {
+    dataContainer.innerHTML += "<p>Data Tidak Ditemukan</p>";
+    return;
+  }
 
-    komikData.forEach((data, index) => {
-        dataContainer.innerHTML += `<p>${data.namaKomik} - Chapter ${data.chapter} <button onclick="hapusData(${index})">Hapus</button></p>`;
-    });
+  data.forEach((item, index) => {
+    dataContainer.innerHTML += `<p>${item.namaKomik} - Chapter ${item.chapter} <button onclick="hapusData(${index})">Hapus</button></p>`;
+  });
 }
 
 function hapusData(index) {
-    komikData.splice(index, 1);
-    updateData();
-    simpanDataLocal();
+  komikData.splice(index, 1);
+  updateData();
+  simpanDataLocal();
 }
 
 function resetForm() {
-    document.getElementById('namaKomik').value = '';
-    document.getElementById('chapter').value = '';
+  document.getElementById("namaKomik").value = "";
+  document.getElementById("chapter").value = "";
 }
 
 function cariData() {
-    const kataKunci = document.getElementById('cariInput').value.toLowerCase();
-    const hasilPencarian = komikData.filter(data =>
-        data.namaKomik.toLowerCase().includes(kataKunci)
-    );
+  const kataKunci = document.getElementById("cariInput").value.toLowerCase();
+  const hasilPencarian = komikData.filter((data) =>
+    data.namaKomik.toLowerCase().includes(kataKunci)
+  );
 
-    komikData.length === 0 ? updateData() : updateData(hasilPencarian);
+  updateData(hasilPencarian);
 }
 
+// function tampilkanSemuaData() {
+//   updateData();
+// }
+
 function simpanDataLocal() {
-    localStorage.setItem('komikData', JSON.stringify(komikData));
+  localStorage.setItem("komikData", JSON.stringify(komikData));
 }
